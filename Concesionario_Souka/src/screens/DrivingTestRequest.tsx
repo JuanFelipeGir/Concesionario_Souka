f028d465c5a5683e71c6e76e0069bc6bcda53525
JuanFelipeGir <juan.giraldo11@correo.tdea.edu.co>
2024-03-16 07:38:00 -0500

    Componentes

---
+++ Concesionario_Souka/src/screens/DrivingTestRequest.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View } from 'react-native'
+
+const DrivingTestRequest = () => {
+  return (
+    <View>
+      
+    </View>
+  )
+}
+
+export default DrivingTestRequest
---
+++ Concesionario_Souka/src/screens/VehiclesCatalog.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View } from 'react-native'
+
+const VehiclesCatalog = () => {
+  return (
+    <View>
+      
+    </View>
+  )
+}
+
+export default VehiclesCatalog
