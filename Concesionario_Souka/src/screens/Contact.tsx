7b7f74974b8d0e5b182a3c3cd5775e6dd01b3d0c
Iumity <camiloqluna@gmail.com>
2024-03-23 17:44:18 -0500

    Changes

--- Concesionario_Souka/src/screens/Contact.tsx
+++ Concesionario_Souka/src/screens/Contact.tsx
@@ -1,13 +1,5 @@
 import React from 'react'
 import { View } from 'react-native'
-<<<<<<< HEAD
-
-const Contact = () => {
-  return (
-    <View>
-      
-    </View>
-=======
 import { PaperProvider, Text } from 'react-native-paper'
 
 const Contact = () => {
@@ -22,7 +14,6 @@ const Contact = () => {
         <Text>ConsecionarioSouka@gmail.com</Text>
       </View>
     </PaperProvider>
->>>>>>> 77a1b35f290ec7b3a2c2f9111dc27465562cdbc2
   )
 }
 
--- Concesionario_Souka/src/screens/DealsAndOffers.tsx
+++ Concesionario_Souka/src/screens/DealsAndOffers.tsx
@@ -1,19 +1,11 @@
 import React from 'react'
-<<<<<<< HEAD
 import { View } from 'react-native'
-
-const DealsAndOffers = () => {
-  return (
-    <View>
-      
-    </View>
-=======
 import CatalogDO from './SubScreens/DealsAndOffers/CatalogDO'
+
 
 const DealsAndOffers = () => {
   return (
     <CatalogDO/>
->>>>>>> 77a1b35f290ec7b3a2c2f9111dc27465562cdbc2
   )
 }
 
